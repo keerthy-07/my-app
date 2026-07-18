f0f747430e8f224d81e993a59e3e12627afab717
Keerthy Vasan <keerthivasankannan76@gmail.com>
2026-07-18 18:19:00 +0530

    updated responsive head,profile and socialbar

--- src/components/header/head.jsx
+++ src/components/header/head.jsx
@@ -1,48 +1,48 @@
 import "./head.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import {FaBars,FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Head(props) {
-  const [menuOpen,setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
-useEffect(() => {
-  const handleScroll = () => {
-    setIsScrolled(window.scrollY > 80);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 80);
+    };
 
-  window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-  return () => {
-    window.removeEventListener("scroll", handleScroll);
-  };
-}, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className={`head ${isScrolled ? "scrolled" : ""}`}>
       <h2>{props.logo}</h2>
-      <div className="menu-icon" 
-      onClick={()=> setMenuOpen(!menuOpen)}>
-        {menuOpen ? <FaTimes />:<FaBars />}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
       <nav className={`menuOpen ? "active : ""}`}>
-        <ul >
+        <ul>
           <li>{props.home}</li>
           <li>{props.about}</li>
           <li>{props.contact}</li>
           <li>{props.docs}</li>
         </ul>
       </nav>
-      
+
       <div className={`buttons ${menuOpen ? "active" : ""}`}>
         <Link to="/Login">
           <button className="log">LOGIN</button>
+
+          <button className="blocks">BLOCKS</button>
         </Link>
-        <button className="blocks">BLOCKS</button>  
       </div>
     </div>
   );
 }
 
-export default Head;        
+export default Head;
