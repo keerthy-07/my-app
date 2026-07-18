5d238ef8243cee3448c5ed7ab24703b4f5ffcbe8
Keerthy Vasan <keerthivasankannan76@gmail.com>
2026-07-18 17:10:13 +0530

    Added responsive hamburger menu

--- src/components/header/head.jsx
+++ src/components/header/head.jsx
@@ -1,9 +1,10 @@
 import "./head.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import {FaBars,FaTimes} from "react-icons/fa";
 
 function Head(props) {
-
+  const [menuOpen,setMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
 useEffect(() => {
@@ -21,15 +22,20 @@ useEffect(() => {
   return (
     <div className={`head ${isScrolled ? "scrolled" : ""}`}>
       <h2>{props.logo}</h2>
-      <nav>
-        <ul className="menu">
+      <div className="menu-icon" 
+      onClick={()=> setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes />:<FaBars />}
+      </div>
+      <nav className={`menuOpen ? "active : ""}`}>
+        <ul >
           <li>{props.home}</li>
           <li>{props.about}</li>
           <li>{props.contact}</li>
           <li>{props.docs}</li>
         </ul>
       </nav>
-      <div className="buttons">
+      
+      <div className={`buttons ${menuOpen ? "active" : ""}`}>
         <Link to="/Login">
           <button className="log">LOGIN</button>
         </Link>
